5091c3a2fcd995480212bf93784866134200f5d9
Juan Francisco Gonzalez <juan.ramos.fg1@gmail.com>
2023-11-05 17:02:16 +0100

    Prepare a simple page for vercel deploy

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,7 +2,6 @@ import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-import Header from "~/components/starter/header/header";
 import Footer from "~/components/starter/footer/footer";
 
 import styles from "./styles.css?inline";
@@ -28,7 +27,6 @@ export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
       <main>
         <Slot />
       </main>
@@ -36,3 +34,10 @@ export default component$(() => {
     </>
   );
 });
+
+/*
+<Header />
+<main>
+  <Slot />
+</main>
+<Footer />*/
